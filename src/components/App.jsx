e0c6ff4976d57fd7744ea9ada2036aee743fa357
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-09-05 18:04:31 +0300

    transfered all code from previous hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Form } from './Form/Form';
+import { List } from './List/List';
+import { Filter } from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix';
+import startingContacts from '../data/contacts.json';
+
+
+export class App extends Component {
+  state = {
+    contacts: startingContacts,
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    if (
+      !this.state.contacts.find(
+        ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+      )
+    ) {
+      this.setState(({ contacts }) => ({
+        contacts: [...contacts, contact],
+      }));
+    } else {
+      return Notify.warning(`${contact.name} is already in contacts!`);
+    }
+
+  };
+
+  removeContact = ID => {
+    this.setState({
+      contacts: this.state.contacts.filter((contact) => contact.id !== ID),
+    });
+  };
+
+  clickOnBtn = () => {
+    console.log("click bbb");
+  };
+
+  onXBtnClick = () => {
+    this.setState({ filter: '' });
+  };
+
+  filterChange = (event) => {
+    this.setState({ filter: event.target.value });
+  };
+
+  showFilteredContacts = () => {
+    const standardFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(standardFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filteredContacts = this.showFilteredContacts();
+
+    return(
+      <>
+      <Section title="Phonebook">
+        <Form onSubmit={this.addContact}></Form>
+      </Section>
+      <Section title="Contacts">
+        <Filter filter={filter} onClear={this.onXBtnClick} onChange={this.filterChange}></Filter>
+        <List data={filteredContacts} onDelete={this.removeContact}></List>
+      </Section>
+      </>
+    )
+  };
 };
